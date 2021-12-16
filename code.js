1649c539440ac0e5efec1ca1847aaa71d3912516
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-16 11:52:38 -0500

    updating jasmine exercises (code.js and tests.js) after completion

--- code.js
+++ code.js
@@ -37,12 +37,49 @@
 //     }
 // }
 
-function isEven(num) {
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function isEven(num) {
+//     if (num % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// function isVowel(input)  {
+//     if (input === 4) {
+//         return false;
+//     }else if (input === true) {
+//         return false;
+//     }else if (input === false) {
+//         return false;
+//     }else if (input === "banana") {
+//         return false;
+//     } else if (input === undefined) {
+//         return false;
+//     }else {  return true;
+//     }
+//
+// }
 
-// console.log(typeof infinity);
+// function isVowel(input) {
+//     if (typeof input !== "string") {
+//         return false;
+//     } else if (input === "a" || input === "A" || input === "e" || input === "E" || input === "i" || input === "I"
+//         || input ==="o" || input ==="O" || input === "u" || input === "U") {
+//         return true;
+//     } {
+//         return false;
+//     }
+// }
+
+// function isVowel(input) {
+//     if (input === "a" || input === "A" || input === "e" || input === "E" || input === "i" || input === "I"
+//         || input ==="o" || input ==="O" || input === "u" || input === "U") {
+//         return true;
+//     }else {
+//         return false;}
+// }
+
+function add(num1, num2) {
+    return parseFloat(num1) + parseFloat(num2);
+}
--- tests.js
+++ tests.js
@@ -67,48 +67,117 @@
 //     });
 // })
 
-describe("isEven", function () {
+// describe("isEven", function () {
+//     it('should be a defined function', function () {
+//         expect(typeof isEven).toBe("function");
+//
+//     });
+//     it('should always return a boolean', function () {
+//         expect(typeof isEven()).toBe("boolean");
+//
+//     });
+//     it('should return true when the input is 2', function () {
+//         expect(isEven(2)).toBe(true);
+//
+//     });
+//     it('should return true when the input is -4', function () {
+//         expect(isEven(-4)).toBe(true);
+//
+//     });
+//     it('should return false when the input is 3', function () {
+//         expect(isEven(3)).toBe(false);
+//
+//     });
+//     it('should return false when the input is banana', function () {
+//         expect(isEven("banana")).toBe(false);
+//
+//     });
+//     it('should return true when the input is 8', function () {
+//         expect(isEven(8)).toBe(true);
+//
+//     });
+//     it('should return false when the input is Infinity', function () {
+//         expect(isEven(Infinity)).toBe(false);
+//
+//     });
+//     it('should return false when input is a boolean', function () {
+//         expect(isEven(true)).toBe(false);
+//
+//     });
+//     it('should return false when not given an input', function () {
+//         expect(isEven()).toBe(false);
+//
+//     });
+// });
+
+// describe("isVowel", function () {
+//     it('should be a defined function', function () {
+//         expect(typeof isVowel).toBe("function");
+//
+//     });
+//     it('should always return a boolean', function () {
+//         expect(typeof isVowel()).toBe("boolean");
+//
+//     });
+//     it('should should return true when the input is "A"', function () {
+//         expect(isVowel("A")).toBe(true);
+//
+//     });
+//     it('should return true when the input is "a"', function () {
+//         expect(isVowel("a")).toBe(true);
+//
+//     });
+//     it('should return false when the input is 4', function () {
+//         expect(isVowel(4)).toBe(false);
+//
+//     });
+//     it('should return false if the input is a boolean', function () {
+//         expect(isVowel(true)).toBe(false);
+//
+//     });
+//     it('should return false if the input is a boolean (false)', function () {
+//         expect(isVowel(false)).toBe(false);
+//
+//     });
+//     it('should return false if the input is "banana"', function () {
+//         expect(isVowel("banana")).toBe(false);
+//
+//     });
+//     it('should return false if there is no input', function () {
+//         expect(isVowel()).toBe(false);
+//
+//     });
+//     it('should return true if the input is "U"', function () {
+//         expect(isVowel("U")).toBe(true);
+//
+//     });
+//     it('should return false if the input is null', function () {
+//         expect(typeof isVowel() === null).toBe(false);
+//
+//     });
+// });
+
+describe("add", function () {
     it('should be a defined function', function () {
-        expect(typeof isEven).toBe("function");
+        expect(typeof add).toBe("function");
 
     });
-    it('should always return a boolean', function () {
-        expect(typeof isEven()).toBe("boolean");
+    it('should return 5 when the inputs are 2 and 3', function () {
+        expect(add(2, 3)).toBe(5);
 
     });
-    it('should return true when the input is 2', function () {
-        expect(isEven(2)).toBe(true);
+    it('should return NaN when one or both of the inputs are strings', function () {
+        expect(isNaN(add("banana", "split"))).toBe(true)
 
     });
-    it('should return true when the input is -4', function () {
-        expect(isEven(-4)).toBe(true);
+    it('should return Nan when no input is given', function () {
+        expect(isNaN(add())).toBe(true);
 
     });
-    it('should return false when the input is 3', function () {
-        expect(isEven(3)).toBe(false);
-
-    });
-    it('should return false when the input is banana', function () {
-        expect(isEven("banana")).toBe(false);
-
-    });
-    it('should return true when the input is 8', function () {
-        expect(isEven(8)).toBe(true);
-
-    });
-    it('should return false when the input is Infinity', function () {
-        expect(isEven(Infinity)).toBe(false);
-
-    });
-    it('should return false when input is a boolean', function () {
-        expect(isEven(true)).toBe(false);
-
-    });
-    it('should return false when not given an input', function () {
-        expect(isEven()).toBe(false);
+    it('should return NaN when one input is a number and the other is a string', function () {
+        expect(isNaN(add(2 + "String"))).toBe(true);
 
     });
 });
-
 
 
